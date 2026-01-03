72b660457b6596fa6b74e1034be172fcc0f291ba
phauvip13 <30490889+phauvip13@users.noreply.github.com>
2026-01-04 06:09:41 +0700

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,6 +2,7 @@ import { organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL as string,
+  // Thay đổi dòng này để linh hoạt hơn
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || window.location.origin,
   plugins: [organizationClient()],
 });
